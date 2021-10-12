0fcfcd0aea3253b6e7748cdd6b10911cfe52745a
RizkyRohmadRR1 <Bagogo1928@gmail.com>
2021-10-12 22:34:12 +0700

    commit keempat

---
+++ application/views/v_tampil.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title> Crud Dengan Codeignater</title>
+    </head>
+<body>
+    <center>
+        <h1>Membuat Sistem CRUD dengan Code Igniter</h1>
+    </center>
+</body>
+</html>
