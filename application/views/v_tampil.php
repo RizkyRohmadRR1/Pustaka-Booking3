25acbaaa718cae55f6579c2b576b7e4d77f56b3a
RizkyRohmadRR1 <Bagogo1928@gmail.com>
2021-10-13 22:16:38 +0700

    CRUD Menampilkan data dari database menggunakan CI

--- application/views/v_tampil.php
+++ application/views/v_tampil.php
@@ -3,8 +3,31 @@
         <title> Crud Dengan Codeignater</title>
     </head>
 <body>
-    <center>
-        <h1>Membuat Sistem CRUD dengan Code Igniter</h1>
-    </center>
+<center><h1>Membuat Sistem CRUD dengan Code Igniter</h1></center>
+<center><?php echo anchor('crud/tambah','Tambah Data'); ?></center>
+    <center><table style="margin 20px auto;" border="1"></center>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Alamat</th>
+                <th>Pekerjaan</th>
+                <th>Action</th>
+            </tr>
+    <?php
+    $no =1;
+    foreach($user as $u){
+    ?>
+        <tr>
+            <td><?php echo $no++?></td>
+            <td><?php echo $u->nama?></td>
+            <td><?php echo $u->alamat?></td>
+            <td><?php echo $u->pekerjaan?></td>
+        <td>
+            <?php echo anchor('crud/edit/' . $u->id, 'Edit');?>
+            <?php echo anchor('crud/hapus/' . $u->id, 'Hapus');?>
+        </td>
+        </tr>
+    <?php } ?>
+    </table>
 </body>
 </html>
